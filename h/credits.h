86425cb9eec833dee63f8b4bed6ae324e2c7bb6a
Chipset <167650100+chipset808@users.noreply.github.com>
2026-04-15 12:55:11 -0400

    Update credits.h

--- h/credits.h
+++ h/credits.h
@@ -1,1 +1,11 @@
+Credits for builds Downloaded.
+Switch Builds: Mostly EZFN
+iOS:
+l.ocalized - Retrieving all builds hosted on Apple's servers.
+Crunnie - Most of the original builds
+mtbr29 - Manifest files, legendary command. 
+Windows: BLK mostly
+Xbox: Xader726
+OT Builds: Fortchive Discord Server
 
+Those credited aren't responsible for all downloads on the archive, but rather a sizeable part of the category they are listed in.
